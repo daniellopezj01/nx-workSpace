d27bc47700d70249114ab054c6d3f2899ea51351
unknown <daniellopezj0327@gmail.com>
2021-10-14 10:30:08 -0500

    begin with test

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -2,14 +2,16 @@ module.exports = {
   displayName: 'api',
   preset: '../../jest.preset.js',
   globals: {
+    ENV: 'test',
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json'
     }
   },
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': 'ts-jest'
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleFileExtensions: ['js', 'ts', 'html'],
   coverageDirectory: '../../coverage/apps/api'
 }
